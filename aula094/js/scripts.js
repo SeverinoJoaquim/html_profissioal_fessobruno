8485e47506b789dfac6a067f1da70f6e4df5d7d2
SeverinoJoaquim <severinojoca@gmail.com>
2019-12-28 19:02:03 -0300

    Aula 94 - Menu de ícones Retrátil

---
+++ aula094/js/scripts.js
@@ -0,0 +1,17 @@
+const menus=document.querySelectorAll('[data-menu]');
+const cssAtivo='ativo';
+
+menus.forEach(function(e){
+	e.addEventListener('click',function(){
+		for(i=0;i<menus.length;i++){
+			menus[i].classList.remove(cssAtivo);
+		}
+		e.classList.add(cssAtivo);
+	})
+});
+
+const btnMenu=document.getElementsByClassName('btnMenu')[0];
+const menu=document.getElementsByClassName('menu')[0];
+btnMenu.addEventListener('click',function(){
+	menu.classList.toggle('menuAberto');	
+});
